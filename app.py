df4aef89ffac467f6ee2b8e8785d3ddc8fd556cd
varun-sahariya <your_email@example.com>
2025-08-30 00:30:50 +0530

    Pin Werkzeug version for Flask compatibility

--- app.py
+++ app.py
@@ -337,4 +337,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
